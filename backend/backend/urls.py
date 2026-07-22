15885b3034fc940071985a99d5b3a222dcba8637
omarjimenez465 <angelomarrjg@gmail.com>
2026-07-22 17:23:16 -0600

    cambiar requeriments

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from clientes import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cliente/', views.cliente)
 ]
